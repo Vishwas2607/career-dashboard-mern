2d18f0664af6bbcffec75e164dfe9b25a3373e3d
Vishwas Kumar <vishwashverma1234@gmail.com>
2026-02-10 13:25:14 +0530

    fix(ui): addApplication and jobDetails

--- frontend/src/pages/addApplication.tsx
+++ frontend/src/pages/addApplication.tsx
@@ -108,26 +108,26 @@ export default function AddApplication() {
 
     return (
         <section className="w-full bg-blue-200 dark:bg-gray-900 transition-all duration-500 ease-in-out flex flex-col min-h-screen items-center">
-            <div className="flex flex-col gap-5 justify-center items-center mt-10 border border-black/30 p-5 min-h-1/2 md:w-1/2 rounded-lg bg-white/30 dark:bg-white/10 dark:border-white/30 backdrop-blur-md shadow-lg hover:backdrop-blur-2xl text-black dark:text-white transition-all duration-300 ease-in-out">
+            <div className="flex flex-col gap-5 justify-center items-center mt-10 border border-black/30 p-5 min-h-1/2 w-[80%] lg:w-1/2 rounded-lg bg-white/30 dark:bg-white/10 dark:border-white/30 backdrop-blur-md shadow-lg hover:backdrop-blur-2xl text-black dark:text-white transition-all duration-300 ease-in-out">
             <h2 className="text-lg md:text-xl font-bold text-black transition-all duration-500 dark:text-white">{isEditing ? "Edit Job Details" : "Add Job Details"}</h2>
-            <form onSubmit={(event)=>handleSubmit(event)} className="flex flex-col flex-wrap gap-5 text-black transition-all duration-500 dark:text-white" key={id ?? "new-application"}>
-            <label htmlFor="company-name" className="text-sm md:text-md">Company Name: 
+            <form onSubmit={(event)=>handleSubmit(event)} className="flex w-full flex-col flex-wrap gap-5 text-black transition-all duration-500 dark:text-white" key={id ?? "new-application"}>
+            <label htmlFor="company-name" className="text-sm md:text-[16px]">Company Name: 
             <input name="company-name" id="company-name" defaultValue={job?.companyName ?? ""} type="text" title="company-name" placeholder="Enter company name" className="input mt-2 md:mt-0 md:ml-2 bg-blue-100 dark:bg-gray-400 dark:border-gray-400"/>
             </label>
-            <label htmlFor="role" className="text-sm md:text-md">Role / Position: 
+            <label htmlFor="role" className="text-sm md:text-[16px]">Role / Position: 
             <input name="role" id="role" type="text" defaultValue={job?.role ?? ""} title="role" placeholder="Enter role" className="input bg-blue-100 dark:bg-gray-400 ml-2 dark:border-gray-400"/>
             </label>
-            <label htmlFor="location" className="text-sm md:text-md">Location:
+            <label htmlFor="location" className="text-sm md:text-[16px]">Location:
             <select name="location" id="location" defaultValue={job?.location ?? "remote"} className="select bg-white dark:bg-gray-700">
                 <option value="remote">Remote</option>
                 <option value="onsite">Onsite</option>
                 <option value="hybrid">Hybrid</option>
             </select>
             </label>
-            <label htmlFor="application-date" className="text-sm md:text-md">Application Date: 
+            <label htmlFor="application-date" className="text-sm md:text-[16px]">Application Date: 
             <input name="application-date" id="application-date" defaultValue={defaultDate} type="date" title="application-date" className="ml-2 input bg-blue-100 dark:bg-gray-400 dark:border-gray-400"/>
             </label>
-            <label htmlFor="status" className="text-sm md:text-md">Status:
+            <label htmlFor="status" className="text-sm md:text-[16px]">Status:
             <select name="status" id="status" defaultValue={job?.status ?? "applied"} className="select bg-white dark:bg-gray-700">
                 <option value="applied">Applied</option>
                 <option value="interviewing">Interviewing</option>
@@ -135,13 +135,13 @@ export default function AddApplication() {
                 <option value="rejected">Rejected</option>
             </select>
             </label>
-            <label htmlFor="job-link" className="text-sm md:text-md">Job Link: 
+            <label htmlFor="job-link" className="text-sm md:text-[16px]">Job Link: 
                 <input type="url" id="job-link" defaultValue={job?.jobLink ?? ""} name="job-link" title="job-link" className="ml-2 input bg-blue-100 dark:bg-gray-400 dark:border-gray-400" placeholder="Enter job link"/>
             </label>
-            <label htmlFor="notes" className="text-sm md:text-md">Notes: 
+            <label htmlFor="notes" className="text-sm md:text-[16px]">Notes: 
                 <textarea id="notes" defaultValue={job?.notes ?? ""} name="notes" title="notes" placeholder="Enter notes..." rows={5} className="mt-2 min-h-6 w-full input bg-blue-100 dark:bg-gray-400 dark:border-gray-400"/>
             </label>
-            {error && <p className="text-sm md:text-md text-red-600 font-semibold text-center dark:text-red-400" role="alert" aria-live="polite">{error}</p>}
+            {error && <p className="text-sm md:text-[16px] text-red-600 font-semibold text-center dark:text-red-400" role="alert" aria-live="polite">{error}</p>}
             <button type="submit" title="add-job" disabled={isPending || isLoading} className="btn btn-primary font-semibold disabled:disabled-btn">{isPending ? "Processing" : (isEditing ? "Update Job" : "Add Job")}</button>
             </form>
             </div>
